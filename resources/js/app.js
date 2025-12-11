d66d2516445c7d4fc51efe9b9ba176c03f9f8fd6
trgiahuy14 <trgiahuy14@gmail.com>
2025-12-11 17:07:42 +0700

    refactor: remove Inertia middleware

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,12 @@
 import '../css/app.css';
 import './bootstrap';
 
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { createApp } from 'vue';
+import router from './router';
+import App from './App.vue';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const app = createApp(App);
 
-createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
-        ),
-    setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue)
-            .mount(el);
-    },
-    progress: {
-        color: '#4B5563',
-    },
-});
+app.use(router);
+
+app.mount('#app');
